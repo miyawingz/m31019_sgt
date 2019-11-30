e60fe489a7d533ecc958af2a0bf3e7e37c608c7b
Mia Zhang <miyawingz@gmail.com>
2019-11-30 10:54:18 -0800

    set up thunk middleware and got grade data from server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,12 @@
 import types from './types';
 import axios from 'axios';
+
+export function getGradeData() {
+    return async function (dispatch) {
+        const resp = await axios.get('/api/grades');
+        dispatch({
+            type: types.GET_GRADE_DATA,
+            grades: resp.data
+        })
+    }
+}
--- src/middleware/thunk.js
+++ src/middleware/thunk.js
@@ -1,1 +1,17 @@
 // Build middleware here
+export default (store) => (next) => (action) => {
+    if (typeof action !== 'function') {
+        return next(action);
+    }
+
+    return action(store.dispatch);
+}
+
+//ES5 Example
+// export default function (store) {
+//     return function (next) {
+//         return function (action) {
+//             //code goes here
+//         }
+//     }
+// }
